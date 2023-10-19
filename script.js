491aa46e692fe9813ec9d3c3fd103849bf1b9d41
Bartosz Glinski <glinskibartosz88@gmail.com>
2023-10-19 12:23:32 +0200

    updates

--- script.js
+++ script.js
@@ -1,52 +1,26 @@
-const clock = document.getElementById('clock');
-const progressCircle = document.getElementById('progress-circle');
-const timer = document.getElementById('timer');
-const startStopButton = document.getElementById('start-stop');
-const resetButton = document.getElementById('reset');
+const timerDisplay = document.getElementById('timer');
+const progressRingCircle = document.getElementById('progress-ring-circle');
+const totalTime = 25 * 60; // 25 minutes in seconds
+let timeLeft = totalTime;
+let timer;
 
-let timeRemaining = 25 * 60; // 25 minutes in seconds
-let interval;
-
-// Start the timer
-startStopButton.addEventListener('click', () => {
-  if (interval) {
-    clearInterval(interval);
-    startStopButton.textContent = 'Start';
-  } else {
-    interval = setInterval(() => {
-      timeRemaining--;
-
-      // Update the progress circle
-      const progress = timeRemaining / (25 * 60);
-      progressCircle.style.strokeDashoffset = 360 - (360 * progress);
-
-      // Update the timer
-      timer.textContent = formatTime(timeRemaining);
-
-      // If the timer reaches zero, stop it and reset it
-      if (timeRemaining === 0) {
-        clearInterval(interval);
-        startStopButton.textContent = 'Start';
-        timer.textContent = '25:00';
-      }
-    }, 1000);
-
-    startStopButton.textContent = 'Stop';
-  }
-});
-
-// Reset the timer
-resetButton.addEventListener('click', () => {
-  clearInterval(interval);
-  timeRemaining = 25 * 60;
-  progressCircle.style.strokeDashoffset = 360;
-  timer.textContent = '25:00';
-});
-
-// Format the time in minutes and seconds
 function formatTime(seconds) {
   const minutes = Math.floor(seconds / 60);
-  const secondsRemaining = seconds % 60;
-
-  return `${minutes}:${secondsRemaining.toString().padStart(2, '0')}`;
+  const remainderSeconds = seconds % 60;
+  return `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
 }
+
+function startTimer() {
+  timer = setInterval(() => {
+    timeLeft--;
+    timerDisplay.textContent = formatTime(timeLeft);
+    const progress = ((totalTime - timeLeft) / totalTime) * 565.48; // 565.48 is the circumference of the circle
+    progressRingCircle.setAttribute('stroke-dasharray', `${progress} 565.48`);
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      // Here you can add code to start a break or restart the work timer
+    }
+  }, 1000);
+}
+
+startTimer();
